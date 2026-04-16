eeb9550c218548ca8123847fea3f88805eec309a
Vineet Singh <vineetnwd99@gmail.com>
2026-04-16 15:03:31 +0530

    Fixed missing config.php requirements causing APP_URL errors

--- api/login.php
+++ api/login.php
@@ -2,6 +2,7 @@
 header('Content-Type: application/json');
 session_start();
 
+require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../core/Database.php';
 require_once __DIR__ . '/../core/Auth.php';
 
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once __DIR__ . '/../config/config.php';
 
 // Autoloading classes
 spl_autoload_register(function ($class) {
